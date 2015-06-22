ece0d6fd4822857feab19423737445a63ad03d30
Matthias Fey <matthias.fey@tu-dortmund.de>
2015-06-22 18:50:21 +0200

    Initial Commit 2.0

---
+++ RSContactGrid/RSContactGrid/RSContactGrid.h
@@ -0,0 +1,19 @@
+//
+//  RSContactGrid.h
+//  RSContactGrid
+//
+//  Created by Matthias Fey on 22.06.15.
+//  Copyright © 2015 Matthias Fey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RSContactGrid.
+FOUNDATION_EXPORT double RSContactGridVersionNumber;
+
+//! Project version string for RSContactGrid.
+FOUNDATION_EXPORT const unsigned char RSContactGridVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RSContactGrid/PublicHeader.h>
+
+
