f52b2a77b3d30302cf14d23ae3a169ffab7097d6
Matthias Fey <matthias.fey@tu-dortmund.de>
2015-07-14 14:58:09 +0200

    added framework to example

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -7,11 +7,15 @@
 //
 
 import UIKit
+import RSContactGrid
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let grid = Grid<SquareElement<Bool, Bool>>()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
